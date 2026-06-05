da19042568f8b95d0385eb665cfc5794997125ad
Lumarysz <230233445+Lumarysz@users.noreply.github.com>
2026-06-06 00:45:34 +0100

    .

--- files/lib/bit-os/bpm/install.py
+++ files/lib/bit-os/bpm/install.py
@@ -1,9 +1,21 @@
 from pathlib import Path
-import subprocess
+import subprocess, portage
+
+vartree = portage.db[portage.root]["vartree"]
+package_match = vartree.dbapi.match("app-misc/fastfetch")[-1]
+use_flags = vartree.dbapi.aux_get(package_match, ["USE"])[0]
+FLAGS =use_flags.split()
+
 def main(package: str):
     gentoodatabase = Path("/var/db/repos/gentoo")
-    if not gentoodatabase.exists():
-        try:
-            subprocess.run(["emerge", "--sync"], check=True)
-        except subprocess.CalledProcessError as e:
-            print(f"Error syncing Gentoo database: {e}")
+    package_categories = []
+    if gentoodatabase.exists():
+        for folder in gentoodatabase.iterdir():
+            if folder.is_dir():
+                package_categories.append(folder.name)
+        for_removal = ["profiles", "metadata", "licenses", "scripts"]
+        for i in for_removal:
+            if i in package_categories:
+                package_categories.remove(i)
+    elif not gentoodatabase.exists():
+        pass
