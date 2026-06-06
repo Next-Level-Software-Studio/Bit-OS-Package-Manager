55ff0c9b845e917286579519384f0915eaf65385
Lumarysz <230233445+Lumarysz@users.noreply.github.com>
2026-06-06 20:04:53 +0100

    .

--- files/lib/bit-os/bpm/install.py
+++ files/lib/bit-os/bpm/install.py
@@ -1,4 +1,4 @@
-import subprocess, os, portage
+import subprocess, os, portage, socket
 dbapi = portage.db[portage.root]["vartree"].dbapi
 try:
     package_match = dbapi.match("sys-apps/bpm")[-1]
@@ -8,42 +8,59 @@ except IndexError:
     FLAGS = []
 
 def main(package_name: str):
-    def pip(name):
-        pass
-    def bpm(name)
-    portagedatabase = "/var/db/repos"
-    available_portage_packages = set()
-    ignore = {"profiles", "metadata", "licenses", "scripts", "eclass"}
-    if os.path.exists(portagedatabase):
+    def internet(timeout=2):
         try:
-            with os.scandir(portagedatabase) as overlays:
-                for overlay in overlays:
-                    if overlay.is_dir():
-                        try:
-                            with os.scandir(overlay.path) as categories:
-                                for cat in categories:
-                                    if cat.is_dir() and cat.name not in ignore:
-                                        try:
-                                            with os.scandir(cat.path) as pkgs:
-                                                for pkg in pkgs:
-                                                    if pkg.is_dir():
-                                                        available_portage_packages.add(f"{cat.name}/{pkg.name}")
-                                        except PermissionError:
-                                            print("Please report, this is bug.")
-                        except PermissionError:
-                            print("Please report, this is bug.")
-        except PermissionError:
-            print("Please report, this is bug.")
-        if (package_name in available_portage_packages) and pip(package_name) and bpm(package_name):
-            print("")
-        elif package_name in available_portage_packages:
-            command = subprocess.run(
-                ["emerge", package_name], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            socket.setdefaulttimeout(timeout)
+            socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect(("8.8.8.8", 53))
+            return True
+        except OSError:
+            return False
+    def bpm(name):
+        pass
+    def portage(name):
+        portagedatabase = "/var/db/repos"
+        available_portage_packages = set()
+        ignore = {"profiles", "metadata", "licenses", "scripts", "eclass"}
+        if os.path.exists(portagedatabase):
+            try:
+                with os.scandir(portagedatabase) as overlays:
+                    for overlay in overlays:
+                        if overlay.is_dir():
+                            try:
+                                with os.scandir(overlay.path) as categories:
+                                    for cat in categories:
+                                        if cat.is_dir() and cat.name not in ignore:
+                                            try:
+                                                with os.scandir(cat.path) as pkgs:
+                                                    for pkg in pkgs:
+                                                        if pkg.is_dir():
+                                                            available_portage_packages.add(f"{cat.name}/{pkg.name}")
+                                                return available_portage_packages
+                                            except PermissionError:
+                                                return False
+                            except PermissionError:
+                                return False
+            except PermissionError:
+                return False
+
+        if package_name in available_portage_packages:
+            existsinportage = True
+        elif package_name not in available_portage_packages:
+            existsinportage = False
+
+        if 
+        
+        if existsinportage is True and bpm(package_name):
+            print("A package is duplicated.")
+        elif existsinportage is True and not bpm(package_name):
+            command = subprocess.run(["emerge", package_name], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
             if command.returncode == 0:
                 print(f"Installing {package_name}, was sucessfull.")
             else:
                 print("Something has gone wrong.")
-        elif pip(package_name):
-
+        elif existsinportage is False and bpm(package_name):
+            pass
+        elif existsinportage is False and not bpm(package_name):
+            print("O pacote não está disponível.")
     elif not os.path.exists(portagedatabase):
         print("Please, create a folder at /var/db/repos")
