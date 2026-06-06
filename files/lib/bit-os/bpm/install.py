8e28f99088b32699963b75faac588651ea68f34a
Lumarysz <230233445+Lumarysz@users.noreply.github.com>
2026-06-06 01:08:50 +0100

    installing module modifications

--- files/lib/bit-os/bpm/install.py
+++ files/lib/bit-os/bpm/install.py
@@ -6,16 +6,16 @@ package_match = vartree.dbapi.match("app-misc/fastfetch")[-1]
 use_flags = vartree.dbapi.aux_get(package_match, ["USE"])[0]
 FLAGS =use_flags.split()
 
-def main(package: str):
+def main(package_name: str):
     gentoodatabase = Path("/var/db/repos/gentoo")
-    package_categories = []
     if gentoodatabase.exists():
-        for folder in gentoodatabase.iterdir():
-            if folder.is_dir():
-                package_categories.append(folder.name)
-        for_removal = ["profiles", "metadata", "licenses", "scripts"]
-        for i in for_removal:
-            if i in package_categories:
-                package_categories.remove(i)
+        ignore = ["profiles", "metadata", "licenses", "scripts"]
+        categories = [p for p in gentoodatabase.iterdir() if p.is_dir() and p.name not in ignore]
+        packages = []
+        for cat in categories:
+            for pkg in cat.iterdir():
+                if pkg.is_dir():
+                    packages.append(f"{pkg.parent.name}/{pkg.name}")
+        print(packages)
     elif not gentoodatabase.exists():
         pass
