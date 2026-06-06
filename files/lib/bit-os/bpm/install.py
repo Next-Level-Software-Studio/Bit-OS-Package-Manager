e8489a49d414fe7037a0fa485c8526c9084f32aa
Lumarysz <230233445+Lumarysz@users.noreply.github.com>
2026-06-06 01:28:34 +0100

    .

--- files/lib/bit-os/bpm/install.py
+++ files/lib/bit-os/bpm/install.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-import subprocess, portage
+import subprocess, portage, os
 
 vartree = portage.db[portage.root]["vartree"]
 package_match = vartree.dbapi.match("app-misc/fastfetch")[-1]
@@ -7,14 +7,25 @@ use_flags = vartree.dbapi.aux_get(package_match, ["USE"])[0]
 FLAGS =use_flags.split()
 
 def main(package_name: str):
-    gentoodatabase = Path("/var/db/repos/gentoo")
-    if gentoodatabase.exists():
-        ignore = ["profiles", "metadata", "licenses", "scripts"]
-        categories = [p for p in gentoodatabase.iterdir() if p.is_dir() and p.name not in ignore]
+    portagedatabase = "/var/db/repos"
+    if os.path.exists(portagedatabase):
         available_portage_packages = []
-        for cat in categories:
-            for pkg in cat.iterdir():
-                if pkg.is_dir():
-                    available_portage_packages.append(f"{pkg.parent.name}/{pkg.name}")
-    elif not gentoodatabase.exists():
+        ignore = {"profiles", "metadata", "licenses", "scripts", "eclass"}
+        with os.scandir(portagedatabase) as overlays:
+            for overlay in overlays:
+                if overlay.is_dir():
+                    try:
+                        with os.scandir(overlay.path) as categories:
+                            for cat in categories:
+                                if cat.is_dir() and cat.name not in ignore:
+                                    try:
+                                        with os.scandir(cat.path) as pkgs:
+                                            for pkg in pkgs:
+                                                if pkg.is_dir():
+                                                    available_portage_packages.append(f"{cat.name}/{pkg.name}")
+                                    except PermissionError:
+                                        continue
+                    except PermissionError:
+                        continue
+    elif not portagedatabase.exists():
         pass
