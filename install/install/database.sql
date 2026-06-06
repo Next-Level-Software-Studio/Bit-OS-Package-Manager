b74e44d85b5930cd5f9202a7cec52ecac9c7fcfb
Lumarysz <230233445+Lumarysz@users.noreply.github.com>
2026-06-06 22:46:21 +0100

    Otimizações relativamente à otimização de espaço ocupado.

--- install/install/database.sql
+++ install/install/database.sql
@@ -12,36 +12,47 @@ CREATE TABLE IF NOT EXISTS package_files (
     FOREIGN KEY (package_id) REFERENCES packages(id) ON DELETE CASCADE
 );
 CREATE TABLE IF NOT EXISTS rdepend (
-    package_id INTEGER NOT NULL,
-    atom       TEXT NOT NULL,
-    FOREIGN KEY (package_id) REFERENCES packages(id) ON DELETE CASCADE
+    package_id    INTEGER NOT NULL,
+    dependency_id INTEGER NOT NULL,
+    FOREIGN KEY (package_id) REFERENCES packages(id) ON DELETE CASCADE,
+    FOREIGN KEY (dependency_id) REFERENCES packages(id) ON DELETE CASCADE
 );
 CREATE TABLE IF NOT EXISTS depend (
-    package_id INTEGER NOT NULL,
-    atom       TEXT NOT NULL,
-    FOREIGN KEY (package_id) REFERENCES packages(id) ON DELETE CASCADE
+    package_id    INTEGER NOT NULL,
+    dependency_id INTEGER NOT NULL,
+    FOREIGN KEY (package_id) REFERENCES packages(id) ON DELETE CASCADE,
+    FOREIGN KEY (dependency_id) REFERENCES packages(id) ON DELETE CASCADE
 );
 CREATE TABLE IF NOT EXISTS idepend (
-    package_id INTEGER NOT NULL,
-    atom       TEXT NOT NULL,
-    FOREIGN KEY (package_id) REFERENCES packages(id) ON DELETE CASCADE
+    package_id    INTEGER NOT NULL,
+    dependency_id INTEGER NOT NULL,
+    FOREIGN KEY (package_id) REFERENCES packages(id) ON DELETE CASCADE,
+    FOREIGN KEY (dependency_id) REFERENCES packages(id) ON DELETE CASCADE
 );
 CREATE TABLE IF NOT EXISTS bdepend (
-    package_id INTEGER NOT NULL,
-    atom       TEXT NOT NULL,
-    FOREIGN KEY (package_id) REFERENCES packages(id) ON DELETE CASCADE
+    package_id    INTEGER NOT NULL,
+    dependency_id INTEGER NOT NULL,
+    FOREIGN KEY (package_id) REFERENCES packages(id) ON DELETE CASCADE,
+    FOREIGN KEY (dependency_id) REFERENCES packages(id) ON DELETE CASCADE
 );
 CREATE TABLE IF NOT EXISTS ndepend (
-    package_id INTEGER NOT NULL,
-    atom       TEXT NOT NULL,
-    FOREIGN KEY (package_id) REFERENCES packages(id) ON DELETE CASCADE
+    package_id    INTEGER NOT NULL,
+    dependency_id INTEGER NOT NULL,
+    FOREIGN KEY (package_id) REFERENCES packages(id) ON DELETE CASCADE,
+    FOREIGN KEY (dependency_id) REFERENCES packages(id) ON DELETE CASCADE
 );
 CREATE TABLE IF NOT EXISTS fdepend (
-    package_id INTEGER NOT NULL,
-    atom       TEXT NOT NULL,
-    FOREIGN KEY (package_id) REFERENCES packages(id) ON DELETE CASCADE
+    package_id    INTEGER NOT NULL,
+    dependency_id INTEGER NOT NULL,
+    FOREIGN KEY (package_id) REFERENCES packages(id) ON DELETE CASCADE,
+    FOREIGN KEY (dependency_id) REFERENCES packages(id) ON DELETE CASCADE
 );
 CREATE INDEX IF NOT EXISTS idx_files_path ON package_files(file_path);
 CREATE INDEX IF NOT EXISTS idx_files_pkg_id ON package_files(package_id);
 CREATE INDEX IF NOT EXISTS idx_rdepend_pkg_id ON rdepend(package_id);
+CREATE INDEX IF NOT EXISTS idx_rdepend_dep_id ON rdepend(dependency_id);
 CREATE INDEX IF NOT EXISTS idx_depend_pkg_id ON depend(package_id);
+CREATE INDEX IF NOT EXISTS idx_idepend_pkg_id ON idepend(package_id);
+CREATE INDEX IF NOT EXISTS idx_bdepend_pkg_id ON bdepend(package_id);
+CREATE INDEX IF NOT EXISTS idx_ndepend_pkg_id ON ndepend(package_id);
+CREATE INDEX IF NOT EXISTS idx_fdepend_pkg_id ON fdepend(package_id);
